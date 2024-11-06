1bc98d0e8e9264e8737a84adce73bd0f2fa76bea
Michelle Cheng <chengmic@oregonstate.edu>
2024-11-05 21:51:54 -0800

    refactor code

--- space_game.py
+++ space_game.py
@@ -6,24 +6,26 @@ from ship import Ship
 class SpaceBattle:
     def __init__(self):
         pygame.init()
+        pygame.display.set_caption("Space Battle")
         self.clock = pygame.time.Clock()
         self.settings = Settings()
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
-        pygame.display.set_caption("Space Battle")
         self.ship = Ship(self)
-
-
 
     def run_game(self):
         while True:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            self._check_events()
+            self._update_screen()
 
-            #rewdraw the screen during each pass through the loop
+    def _check_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+    def _update_screeen(self):
             self.screen.fill(self.settings.bg_color)
-            
             self.ship.blitme()
+            
             # make most recently drawn screen visible
             pygame.display.flip()
             self.clock.tick(60)
